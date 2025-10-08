027390a82a542f6339886d0ed6f04ca4d4e8ecee
unknown <PunnawithSut>
2025-10-08 22:35:16 +0900

    fix: linting

--- server/server.py
+++ server/server.py
@@ -2,9 +2,11 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return jsonify({"message": "Server is running!"})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
