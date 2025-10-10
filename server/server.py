ff0b75ea23b718b3da20b45cd6803be2764c29fc
PunnawithSut <punnawith05@gmail.com>
2025-10-10 22:11:07 +0900

    add: connectivity to database

--- server/server.py
+++ server/server.py
@@ -1,6 +1,16 @@
 from flask import Flask, jsonify
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+
+client = MongoClient(os.getenv("MONGO_URI"))
+db = client["ComfortZone"]
+collection = db["SensorData"]
 
 
 @app.route("/", methods=["GET"])
@@ -12,5 +22,16 @@ def index():
 def getData():
     return jsonify({"success": False, "message": "Successfully get sensor's data"})
 
+
+@app.route("/check_connection", methods=["GET"])
+def check_connection():
+    try:
+        client.admin.command("ping")
+        return jsonify({"success": True, "message": "Connected to MongoDB Atlas!"})
+    except Exception as e:
+        return jsonify({"success": False, "error": str(e)})
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
