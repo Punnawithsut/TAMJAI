6a0169e56ea71f491fbdf8c4db06b123c268a8f9
PunnawithSut <punnawith05@gmail.com>
2025-10-16 22:36:07 +0900

    add: nav bar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom"
+
 const NavBar = () => {
   return (
-    <div className="w-full h-15 bg-blue-500">
+    <div className="w-full h-20 bg-[#1800ad] flex justify-between items-center px-4">
       <div>
-        <h1>Comfort Zone</h1>
+        <h1 className="text-2xl font-bold text-white">Comfort Zone</h1>
       </div>
-      <div>
-        
+      <div className="flex gap-20 mr-20">
+        <Link to="/" className="text-white hover:underline">Home</Link>
+        <Link to="/dashboard" className="text-white hover:underline">Dashboard</Link>
+        <Link to="/ai" className="text-white hover:underline">AI</Link>
       </div>
     </div>
   )
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,13 @@
+import { useContext } from "react"
+import { InformationContext } from "../contexts/InformationContext"
+
 const Dashboard = () => {
+  const { temp, humidity, lux, windowStatus, setWindowStatus } = useContext(InformationContext);
+
   return (
-    <div>Dashboard</div>
+    <div className="w-full min-h-screen bg-[#f1f1f1]">
+      Dashboard
+    </div>
   )
 }
 
