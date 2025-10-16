2c8478832e243889fb5bc5334fe49f9a1bf7e031
PunnawithSut <punnawith05@gmail.com>
2025-10-16 09:41:20 +0900

    fix: frontend API

--- client/src/contexts/InformationProvider.jsx
+++ client/src/contexts/InformationProvider.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import toast from "react-hot-toast"
 import { InformationContext } from "./InformationContext";
 
-const baseUrl = "http://127.0.0.1:5000";
+const baseUrl = "https://comfortzone-backend.onrender.com";
 axios.defaults.baseURL = baseUrl;
 
 export const InformationProvider = ({ children }) => {
